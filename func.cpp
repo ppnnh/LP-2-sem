483ad9837731bb58a0e24d5b96789310add77a68
lerochka <76012407+ppnnh@users.noreply.github.com>
2021-09-30 11:27:20 +0300

    Add files via upload

---
+++ func.cpp
@@ -0,0 +1,28 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+char* UpperW1251(char* destination, char* source)
+{
+	for (int i = 0; i < strlen(source); i++)
+		if (((source[i] >= 97) && (source[i] <= 122)) || ((source[i] >= -32) && (source[i] <= -1)))
+			destination[i] = source[i] - 32;
+		else destination[i] = source[i];
+	return destination;
+}
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	char source[] = "12345sdfghjйцгшщз";
+	char destination[] = "124567890123456789";
+
+	UpperW1251(destination, source);
+
+	cout << destination << endl;
+	system("pause");
+	return 0;
+}
